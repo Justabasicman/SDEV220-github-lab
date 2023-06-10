b8d6f1e313dd2c3cd838c9d39669bf41daa29c14
Joshua Curtiss <curtiss.joshua@student.greenriver.edu>
2023-06-10 09:14:54 -0700

    Committing driver file.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Joshua Curtiss
+ * @version 1.0
+ */
+
+public class Main
+{
+    /**
+     * Stars the application
+     * @param args command-line args(not used)
+     */
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!, from SDEV 220!");
     }
 }
